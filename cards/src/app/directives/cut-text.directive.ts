3ae082fd1d3f92c72c20b0c3cb1078cc3d3f1fa1
AntonKozlov99 <antonk@asist-lab.com>
2023-08-08 18:31:07 +0300

    Add directive with streams

--- cards/src/app/directives/cut-text.directive.ts
+++ cards/src/app/directives/cut-text.directive.ts
@@ -1,33 +1,51 @@
-import { Directive, ElementRef, Input, OnChanges, Renderer2, SimpleChanges } from '@angular/core';
+import { map, Subject, take } from 'rxjs';
+import { Directive, ElementRef, Input, OnChanges, OnDestroy, OnInit, Renderer2, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[appCutText]'
 })
-export class CutTextDirective implements OnChanges {
+export class CutTextDirective implements OnInit, OnChanges, OnDestroy {
 
   @Input() appCutText = '';
-  
-  constructor(private el: ElementRef,private renderer: Renderer2) {
 
+  readonly cutText$ = new Subject<string>();
+  
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.cutText$
+      .pipe(
+        map(text => {
+          if (text.length > 200) {
+            const span = this.renderer.createElement('span'); 
+            this.renderer.addClass(span, 'expandText');
+            const spanText = this.renderer.createText('...');
+      
+            this.el.nativeElement.textContent = text.slice(0, 200);
+            this.renderer.appendChild(span, spanText);
+            this.renderer.appendChild(this.el.nativeElement, span);
+          }
+          return text;
+        }),
+      )
+      .subscribe()
+  }
+
+  ngOnDestroy(): void {
+    this.cutText$.unsubscribe();
   }
 
   ngOnChanges(changes: SimpleChanges) {
-
-    if (changes['appCutText'].currentValue && changes['appCutText'].currentValue !== changes['appCutText'].previousValue) {
-      this.addPointsAndCutText(changes['appCutText'].currentValue);
-    }
- 
-  }
-
-  addPointsAndCutText(text: string) {
-    if (text.length > 200) {
-      const span = this.renderer.createElement('span'); 
-      this.renderer.addClass(span, 'expandText');
-      const spanText = this.renderer.createText('...');
-
-      this.el.nativeElement.textContent = text.slice(0, 200);
-      this.renderer.appendChild(span, spanText);
-      this.renderer.appendChild(this.el.nativeElement, span);
+    if (
+      changes['appCutText'].currentValue && 
+      changes['appCutText'].currentValue !== changes['appCutText'].previousValue
+    ) {
+      this.cutText$.next(changes['appCutText'].currentValue);
     }
   }
 }
